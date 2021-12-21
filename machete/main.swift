258c2892df061c2e5720f53f73bede23b915aacb
Masahiro Oono <words.ssp@gmail.com>
2021-12-21 12:44:02 +0900

    Add loadText method

--- machete/main.swift
+++ machete/main.swift
@@ -23,15 +23,28 @@ class Machete {
     case syntaxError(Character)
   }
 
-  var text = """
-     a=1;
-     b=2;
-     c=a+b;
-     print c;
-     """
+  var text = ""
   var vars = [Int](repeating: 0, count: 256) // 変数
 
+  func loadText(_ args: [String]) {
+    if args.count < 2 {
+      print("Usage: \((args[0] as NSString).lastPathComponent) program-file")
+      exit(1)
+    }
+
+    do {
+      text = try String(contentsOf: URL(fileURLWithPath: args[1]))
+    }
+    catch {
+      print("Failed to open \(args[1])")
+      exit(1)
+    }
+  }
+
   func run() throws {
+    let args = CommandLine.arguments
+    loadText(args)
+
     for c in [UInt8]("0123456789".utf8) {
       vars[Int(c)] = Int(c) - 48
     }
